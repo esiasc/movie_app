c66d715e9153cc75b868ae67b11c313442400b59
esiasc <esiascd@gmail.com>
2021-10-07 02:42:02 +0900

    2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -2,40 +2,48 @@
 function Food({name, picture}){
   return <div>
     <h2>I like {name}</h2>
-    <img src={picture} />
+    <img src={picture} alt={name} />
   </div>
 }
 // name, picture 두 prop을 전달 받아온다.
+// id가 key prop으로 존재는 하지만 데이터를 전달받진 않는다.
 
 const foodILike = [
   {
+    id: 1,
     name: "Taco",
     image:
       "https://images-gmi-pmc.edge-generalmills.com/e59f255c-7498-4b84-9c9d-e578bf5d88fc.jpg"
   },
   {
+    id: 2,
     name: "Pizza",
     image:
       "https://barpizzaque.com/wp-content/uploads/2021/05/Cheese-Pizza-1024x768.gif"
   },
   {
+    id: 3,
     name: "Salad",
     image:
       "https://www.twopeasandtheirpod.com/wp-content/uploads/2019/06/Easy-Green-Salad-4.jpg"
   },
   {
+    id: 4,
     name: "Hot Pot",
     image:
       "https://www.chatelaine.com/wp-content/uploads/2021/02/CHE02_WEB_FD_HOTPOT_ArticleFeature.jpg"
   }
 ]
 
+function renderFood(dish){
+  console.log(dish);
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />
+}
+
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
