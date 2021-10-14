e79d3ad24e0fb48fef7a29cd35173b79831d819b
esiasc <esiascd@gmail.com>
2021-10-14 23:38:17 +0900

    3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -3,24 +3,27 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
+  };
+  // 미래에 사용하고자 하는 state를 선언하는건 필수가 아니다. 미래를 위해 계획하는 것 뿐이다. 나중에 state를 추가해도 된다. (처음에 선언해두는 것이 좋은 습관이긴 함)
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    }, 6000);
   }
-  add = () => {
-    this.setState(current => ({count: current.count + 1}))
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}))
-  };
+  // 6초 후에 isLoading이 false로 바뀐다.
+  // componentDidMount에서 data를 fetch한다
+  
   render(){
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+        {isLoading ? "Loading..." : "We are ready"}
       </div>
       );
   }
 }
-
+// es6
 
 export default App;
