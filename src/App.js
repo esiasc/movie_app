9eeef0e08aaea54ca0d82d0beacd054b409122ec
esiasc <esiascd@gmail.com>
2021-10-15 01:47:41 +0900

    4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -7,13 +7,13 @@ class App extends React.Component {
     movies: []
   };
   // 미래에 사용하고자 하는 state를 선언하는건 필수가 아니다. 미래를 위해 계획하는 것 뿐이다. 나중에 state를 추가해도 된다. (처음에 선언해두는 것이 좋은 습관이긴 함)
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+  };
+  
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false});
-    }, 6000);
+    this.getMovies();
   }
-  // 6초 후에 isLoading이 false로 바뀐다.
-  // componentDidMount에서 data를 fetch한다
   
   render(){
     const { isLoading } = this.state;
